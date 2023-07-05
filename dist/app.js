59f1c3652d1a3a7b9fd56875dc7d17d153a8889f
Mert Çetin <mrtctn506@gmail.com>
2023-07-05 13:57:27 +0300

    create yapildi

---
+++ dist/app.js
@@ -0,0 +1,35 @@
+const lengthDOM = document.querySelector('.length')
+const upperCaseDOM = document.querySelector('.upperCase')
+const lowerCaseDOM = document.querySelector('.lowerCase')
+const numsDOM = document.querySelector('.nums')
+const symbolDOM = document.querySelector('.symbol')
+
+const createDOM = document.querySelector('.create')
+const copyDOM = document.querySelector('.copy')
+const inputDOM = document.querySelector('.input')
+
+function PasswordGenerator() {
+
+    const length = lengthDOM.value;
+    const useUpperCase = upperCaseDOM.checked;
+    const useLowerCase = lowerCaseDOM.checked;
+    const useNums = numsDOM.checked;
+    const useSymbol = symbolDOM.checked;
+
+    let result = '';
+    if (useUpperCase) result += 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+    if (useLowerCase) result += 'abcdefghijklmnopqrstuvwxyz';
+    if (useNums) result += '0123456789';
+    if (useSymbol) result += '!@#$%^&*()';
+
+    let password = '';
+    for (let i = 0; i < length; i++) {
+        const randomIndex = Math.floor(Math.random() * result.length);
+        password += result[randomIndex];
+    }
+
+    inputDOM.value = password;
+}
+
+createDOM.addEventListener('click', PasswordGenerator);
+
